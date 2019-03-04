9072130372b8a56e4a761aa6fb9e02f07586da46
iliaess <47033667+iliaess@users.noreply.github.com>
2019-03-04 12:24:06 +0100

    Add files via upload

---
+++ Factory/src/main/java/projet_Factory/Factory/FactoryApplication.java
@@ -0,0 +1,13 @@
+package projet_Factory.Factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryApplication.class, args);
+	}
+
+}
