22e115025e236a666380673068b507e81f03d7ef
iliaess <iliaess.faress@gmail.com>
2019-03-05 10:29:03 +0100

    restModule+restPromo

---
+++ Factory/src/main/java/factory/restcontroller/ModuleRestController.java
@@ -0,0 +1,96 @@
+package factory.restcontroller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import factory.entity.Module;
+import factory.entity.Ordinateur;
+import factory.entity.Projecteur;
+import factory.entity.Salle;
+import factory.jsonviews.JsonViews;
+import factory.repository.ModuleRepository;
+
+@RestController
+@RequestMapping("/module")
+public class ModuleRestController {
+
+	@Autowired
+	ModuleRepository moduleRepository;
+	
+	@JsonView(JsonViews.CommonModule.class)
+	@GetMapping(value= {"", "/"})
+	public ResponseEntity<List<Module>> list() {
+		return new ResponseEntity<List<Module>>(moduleRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@JsonView(JsonViews.CommonModule.class)
+	@GetMapping("/{id}")
+	public ResponseEntity<Module> findById(@PathVariable(name="id") Long id){
+		Optional<Module> opt = moduleRepository.findById(id);
+		if(opt.isPresent()) {
+			return new ResponseEntity<Module>(opt.get(),HttpStatus.OK);
+		}
+		return new ResponseEntity<Module>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PostMapping(value = {"","/"})
+	public ResponseEntity<Void> insert(@Valid @RequestBody Module module, BindingResult br, UriComponentsBuilder uCB) {
+		if(br.hasErrors()) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		moduleRepository.save(module); 
+		URI uri = uCB.path("/module/{id}").buildAndExpand(module.getId()).toUri();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(uri);
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Module> update(@Valid @RequestBody Module module, BindingResult br, @PathVariable(name="id") Long id){
+		Optional<Module> opt = moduleRepository.findById(id);
+		if(opt.isPresent()) {
+			Module moduleEnBase = opt.get();
+			moduleEnBase.setDebut(module.getDebut());
+			moduleEnBase.setFin(module.getFin());
+			moduleEnBase.setFormateur(module.getFormateur());
+			moduleEnBase.setMatiere(module.getMatiere());
+
+			moduleRepository.save(moduleEnBase);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable(name="id") Long id){
+		Optional<Module> opt = moduleRepository.findById(id);
+		if(opt.isPresent()) {
+			moduleRepository.delete(opt.get());
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+}
+
---
+++ Factory/src/main/java/factory/restcontroller/PromoRestController.java
@@ -0,0 +1,110 @@
+package factory.restcontroller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import factory.entity.Promo;
+import factory.entity.Stagiaire;
+import factory.jsonviews.JsonViews;
+import factory.repository.PromoRepository;
+
+@RestController
+@RequestMapping("/promo")
+public class PromoRestController {
+
+	@Autowired
+	private PromoRepository promoRepository;
+
+	@JsonView(JsonViews.CommonPromo.class)
+	@GetMapping(value = { "", "/" })
+	public ResponseEntity<List<Promo>> list() {
+		return new ResponseEntity<List<Promo>>(promoRepository.findAll(), HttpStatus.OK);
+	}
+
+////	@JsonView(JsonViews.promoWithStagiaire.class)
+//	@GetMapping("/Stagiaires")
+//	public ResponseEntity<List<Promo>> listWithStagiaire() {
+//		return new ResponseEntity<List<Promo>>(promoRepository.findAll(), HttpStatus.OK);
+//	}
+
+	@PostMapping(value = { "", "/" })
+	public ResponseEntity<Void> insert(@Valid @RequestBody Promo promo, BindingResult br, UriComponentsBuilder uCB) {
+		if (br.hasErrors()) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		promoRepository.save(promo);
+		URI uri = uCB.path("/rest/promo/{id}").buildAndExpand(promo.getId()).toUri();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(uri);
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	}
+
+	@JsonView(JsonViews.CommonPromo.class)
+	@GetMapping("/{id}")
+	public ResponseEntity<Promo> findById(@PathVariable(name = "id") Long id) {
+		Optional<Promo> opt = promoRepository.findById(id);
+		if (opt.isPresent()) {
+			return new ResponseEntity<Promo>(opt.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Promo>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+////@JsonView(JsonViews.promoWithStagiaire.class)
+//	@GetMapping("/{id}/Stagiaire")
+//	public ResponseEntity<Promo> findByIdWithStagiaire(@PathVariable(name = "id") Long id) {
+//		Optional<Promo> opt = promoRepository.findById(id);
+//		if (opt.isPresent()) {
+//			return new ResponseEntity<Promo>(opt.get(), HttpStatus.OK);
+//		} else {
+//			return new ResponseEntity<Promo>(HttpStatus.NOT_FOUND);
+//		}
+//	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody Promo promo, BindingResult br,
+			@PathVariable(name = "id") Long id) {
+		Optional<Promo> opt = promoRepository.findById(id);
+		if (opt.isPresent()) {
+			Promo promoEnBase = opt.get();
+			promoEnBase.setListStagiaire(promo.getListStagiaire());
+			promoEnBase.setListModule(promo.getListModule());
+			promoRepository.save(promoEnBase);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable(name = "id") Long id) {
+		Optional<Promo> opt = promoRepository.findById(id);
+		if (opt.isPresent()) {
+			promoRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+}
